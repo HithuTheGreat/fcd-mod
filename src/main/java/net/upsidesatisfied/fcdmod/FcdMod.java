46bfc883f2dfceefe7ebebd67a193a071caee70f
hithaysha <104015375+hithaysha@users.noreply.github.com>
2024-09-21 19:37:23 +0530

    Added "Block of Pink Garnet" (pink_garnet_block) block

--- src/main/java/net/upsidesatisfied/fcdmod/FcdMod.java
+++ src/main/java/net/upsidesatisfied/fcdmod/FcdMod.java
@@ -2,6 +2,7 @@ package net.upsidesatisfied.fcdmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.upsidesatisfied.fcdmod.block.ModBlocks;
 import net.upsidesatisfied.fcdmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class FcdMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
